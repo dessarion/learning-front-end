0d90e63bdf788be1abc99623baf698af335f9734
dessarion <dessuas@gmail.com>
2020-05-01 19:37:16 +0300

    added javascript to menu

--- web_pages/tesla_landpage/js/main.js
+++ web_pages/tesla_landpage/js/main.js
@@ -5,4 +5,13 @@ $(function(){
         autoplay: 3000,
         dots: true,
     });
+
+    $('.header-btn').on('click', function(){
+        $('.menu').addClass('active');
+        
+    });
+
+    $('.close-btn').on('click', function(){
+        $('.menu').removeClass('active');
+    })
 });
